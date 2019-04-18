46db0bdfbaaae1f668b46b9356a2eb536b1661d3
Yoolee <723269147@qq.com>
2019-04-18 10:57:44 +0800

    实现记住密码功能。

--- app/src/main/java/com/example/loginpractice/LoginActivity.java
+++ app/src/main/java/com/example/loginpractice/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.example.loginpractice;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -17,6 +20,8 @@ public class LoginActivity extends AppCompatActivity {
 
     private EditText accountEdit;
     private EditText passwordEdit;
+    private SharedPreferences pref;
+    private CheckBox rememberPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +29,27 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         accountEdit = findViewById(R.id.account);
         passwordEdit = findViewById(R.id.password);
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        rememberPass = findViewById(R.id.remember_pass);
         Button login = findViewById(R.id.login_button);
+        boolean isRemember = pref.getBoolean("remember_password", false);
+        if (isRemember) {
+            // 将账号密码都设置到文本框中
+            String account = pref.getString("account", "");
+            String password = pref.getString("password", "");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberPass.setChecked(true);
+        }
         // 创建数据库并导入管理员账号
         Utility.setManager();
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String account = accountEdit.getText().toString();
-                String password = getMD5String(passwordEdit.getText().toString());
-                if(Utility.CheckLogin(account, password)) {
+                String password = passwordEdit.getText().toString();
+                if(Utility.CheckLogin(account, getMD5String(password))) {
+                    CheckRemember(account, password);
                     Intent intent = new Intent(LoginActivity.this, LoginSuccess.class);
                     startActivity(intent);
                     finish();
@@ -42,6 +59,18 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public void CheckRemember(String account, String password) {
+        SharedPreferences.Editor editor = pref.edit();
+        if (rememberPass.isChecked()) {  // 检查复选框是否被选中
+            editor.putBoolean("remember_password", true);
+            editor.putString("account", account);
+            editor.putString("password", password);
+        } else {
+            editor.clear();
+        }
+        editor.apply();
     }
 
     public static String getMD5String(String str) {
